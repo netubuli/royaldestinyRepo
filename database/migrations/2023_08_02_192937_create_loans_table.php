c4a3de3b1e85d2f1dddc2b8cbc97144c3c0c5981
Nelson <netubuli@gmail.com>
2023-08-10 19:36:30 +0300

    published initial project state

---
+++ database/migrations/2023_08_02_192937_create_loans_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('loans', function (Blueprint $table) {
+            $table->id();
+            $table->string('member_number');
+            $table->string('loan_type');
+            $table->double('amount');
+            $table->string('repayment_period');
+            $table->string('balance');
+            $table->string('interest_rate');
+            $table->string('repayment_status');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('loans');
+    }
+};
